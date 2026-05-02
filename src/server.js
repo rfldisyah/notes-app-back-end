eb784a367a6e363f969d341cc04cf1dcb917154c
Refaldi Julidinsyah <syahrefaldi@gmail.com>
2026-05-02 14:00:04 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
-const host =process.env.NODE_ENV === 'production' ? 'localhost' : '0.0.0.0';
+const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
 
 app.use(cors({
   origin: '*',
